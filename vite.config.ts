e45e93d938b915464f63c5b390fc43748a2bbe95
aivaras <aivarasbankauskas@gmail.com>
2023-11-28 17:47:38 +0200

    .eslintrc.cjs: add eslint to project

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
-        vue()
-    ],
-    server: {
-        port: 5174
-    },
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    }
+	plugins: [
+		vue()
+	],
+	server: {
+		port: 5174
+	},
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url))
+		}
+	}
 });
