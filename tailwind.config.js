99d7c4a9202f1e4ae81a57dd4d7a8946d7d8ed1d
aivaras <aivarasbankauskas@gmail.com>
2023-11-29 18:40:50 +0200

    App.vue: implementing home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,26 +5,22 @@ export default {
 		'./src/**/*.{vue,js,ts,jsx,tsx}'
 	],
 	theme: {
-		colors: {
-			'body-bg': '#e5e7eb',
-			'background': '#ffffff',
-			'color': '#111827',
-			'hover': '#4f46e5',
-			'primary': '#4f46e5',
-			'primary-light': '#a5b4fc',
-			'secondary': '#9ca3af',
-			'success': '#22c55e',
-			'danger': '#ec4899',
-			'danger-light': '#f9a8d4',
-			'warning': '#f97316',
-			'info': '#0284c7',
-			'gray-dark': '#273444',
-			'gray': '#8492a6',
-			'gray-light': '#e5e7eb',
-			'black': '#111827',
-			'white': '#f8fafc'
-		},
-		extend: {}
+		extend: {
+			colors: {
+				'primary': '#4f46e5',
+				'primary-light': '#a5b4fc',
+				'secondary': '#9ca3af',
+				'success': '#22c55e',
+				'danger': '#ec4899',
+				'danger-light': '#f9a8d4',
+				'warning': '#f97316',
+				'info': '#0284c7',
+				'gray-dark': '#3f3f46',
+				'gray': '#52525b',
+				'gray-light': '#e4e4e7'
+			  }
+		}
 	},
+	darkMode: 'class',
 	plugins: []
 };
