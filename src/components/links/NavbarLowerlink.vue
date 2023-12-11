b781a07027174e73bf51c0e675dd8798de9f9042
aivaras <aivarasbankauskas@gmail.com>
2023-12-11 12:28:48 +0200

    SidebarMenu.vue: add navbar lower link component

---
+++ src/components/links/NavbarLowerlink.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+    defineProps<{
+        title: string,
+        linkTo: string,
+    }>();
+</script>
+
+<template>
+    <RouterLink class="flex gap-x-3 justify-start items-center text-sm leading-5" :to="linkTo">
+        <slot />
+        <span>{{ title }}</span>
+    </RouterLink>
+</template>
