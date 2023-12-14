08ffa88746e32cf448fd32bb9675136aa20c9f0a
aivaras <aivarasbankauskas@gmail.com>
2023-12-14 16:32:54 +0200

    HeaderLayout.vue: update default colors

--- src/views/layouts/HeaderLayout.vue
+++ src/views/layouts/HeaderLayout.vue
@@ -25,8 +25,7 @@
     <div class="hidden lg:flex">
         <CompanyNameLink :data="data" link-to="/" span-class="text-2xl" />
     </div>
-    <div class="fixed inset-x-0 top-0 z-50 flex h-14 items-center justify-between gap-4 px-4 border-b border-gray-light dark:border-gray-dark transition sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80 backdrop-blur-sm dark:backdrop-blur bg-light/[var(--bg-opacity-light)] dark:bg-dark/[var(--bg-opacity-dark)]">
-        <div class="absolute inset-x-0 top-full h-px transition bg-zinc-900/7.5 dark:bg-light/7.5"></div>
+    <div class="fixed inset-x-0 top-0 z-50 flex h-14 items-center justify-between gap-4 px-4 shadow-md shadow-black/5 dark:shadow-white/5 transition sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80 backdrop-blur-sm dark:backdrop-blur bg-white/[var(--bg-opacity-light)] dark:bg-black/[var(--bg-opacity-dark)]">
         <div class="flex items-center gap-5 lg:hidden">
             <HamburgerIcon />
             <CompanyNameLink :data="data" link-to="/" :class="{ 'hidden': toggleStore.isSearchShown }" span-class="text-xl" />
@@ -36,15 +35,15 @@
         </div>
         <div class="flex items-center gap-5">
             <nav class="hidden lg:block">
-                <ul role="list" class="flex items-center gap-8 text-zinc-600 dark:text-zinc-400">
+                <ul role="list" class="flex items-center gap-8 text-color-light/70 dark:text-color-dark/70">
                     <NavigationLink title="API" link-to="/api" base-class="text-sm leading-5" />
                     <NavigationLink title="Documentation" link-to="/documentation" base-class="text-sm leading-5" />
                 </ul>
             </nav>
-            <div class="hidden lg:block md:h-5 md:w-px md:bg-gray-light md:dark:bg-gray-dark"></div>
+            <div class="hidden lg:block md:h-5 md:w-px md:bg-divider-light md:dark:bg-divider-dark"></div>
             <div class="flex gap-4">
                 <SearchIcon />
-                <ThemeIcon class="theme-icon-class" light-class="stroke-zinc-900" dark-class="stroke-white" />
+                <ThemeIcon class="theme-icon-class" light-class="stroke-color-light" dark-class="stroke-color-dark" />
                 <NotificationIcon :class="{ 'hidden': toggleStore.isSearchShown }" />
             </div>
             <UserAvatar :data="data" />
@@ -54,6 +53,6 @@
 
 <style scoped>
     .theme-icon-class {
-        @apply hidden md:flex items-center justify-center h-6 w-6 rounded-md transition hover:bg-zinc-900/5 dark:hover:bg-light/5;
+        @apply hidden md:flex items-center justify-center h-6 w-6 rounded-md transition hover:bg-black/5 dark:hover:bg-white/10;
     }
 </style>
