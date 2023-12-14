9b6778022615a70b2b7d31653d8428ccb87e64c8
aivaras <aivarasbankauskas@gmail.com>
2023-12-14 11:17:16 +0200

    ProjectCard.vue: update default colors

--- src/components/cards/ProjectCard.vue
+++ src/components/cards/ProjectCard.vue
@@ -7,23 +7,25 @@
 </script>
 
 <template>
-    <article class="relative rounded-2xl px-4 pb-4 pt-16">
-        <div class="flex h-9 w-9 items-center justify-center rounded-full bg-light ring-1 ring-gray-light dark:ring-gray-dark backdrop-blur-[2px] transition duration-300 group-hover:bg-light/50 group-hover:ring-gray-dark dark:bg-dark dark:text-zinc-400 dark:ring-white/15 dark:group-hover:bg-dark dark:group-hover:ring-gray-light">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" aria-hidden="true">
-                <title>Project</title>
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 00-1.883 2.542l.857 6a2.25 2.25 0 002.227 1.932H19.05a2.25 2.25 0 002.227-1.932l.857-6a2.25 2.25 0 00-1.883-2.542m-16.5 0V6A2.25 2.25 0 016 3.75h3.879a1.5 1.5 0 011.06.44l2.122 2.12a1.5 1.5 0 001.06.44H18A2.25 2.25 0 0120.25 9v.776" />
-            </svg>
+    <article class="group relative flex rounded-2xl bg-light dark:bg-dark transition-shadow hover:shadow-md shadow-shadow-light dark:shadow-shadow-dark">
+        <div class="relative rounded-2xl px-4 pb-4 pt-16">
+            <div class="flex h-9 w-9 items-center justify-center rounded-full text-link-light dark:text-link-dark ring-1 ring-link-light dark:ring-link-dark transition duration-300 group-hover:ring-link-dark dark:group-hover:ring-link-light">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" aria-hidden="true">
+                    <title>Project</title>
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 00-1.883 2.542l.857 6a2.25 2.25 0 002.227 1.932H19.05a2.25 2.25 0 002.227-1.932l.857-6a2.25 2.25 0 00-1.883-2.542m-16.5 0V6A2.25 2.25 0 016 3.75h3.879a1.5 1.5 0 011.06.44l2.122 2.12a1.5 1.5 0 001.06.44H18A2.25 2.25 0 0120.25 9v.776" />
+                </svg>
+            </div>
+            <header>
+                <h3 class="mt-4 text-sm font-semibold leading-7 text-color-light dark:text-color-dark">
+                    <router-link :to="linkTo">
+                        <span class="absolute inset-0 rounded-2xl"></span>
+                        {{ title }}
+                    </router-link>
+                </h3>
+            </header>
+            <section>
+                <p class="mt-1 text-sm text-link-light dark:text-link-dark">{{ description }}</p>
+            </section>
         </div>
-        <header>
-            <h3 class="mt-4 text-sm font-semibold leading-7 text-zinc-900 dark:text-white">
-                <router-link :to="linkTo" class="focus:outline-none focus:ring focus:border-blue-300">
-                    <span class="absolute inset-0 rounded-2xl"></span>
-                    {{ title }}
-                </router-link>
-            </h3>
-        </header>
-        <section>
-            <p class="mt-1 text-sm text-zinc-600 dark:text-zinc-400">{{ description }}</p>
-        </section>
     </article>
 </template>
