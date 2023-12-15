033e0437f6a29a38167655a0f381bc1bf2d29b73
aivaras <aivarasbankauskas@gmail.com>
2023-12-15 10:46:24 +0200

    SectionLayout.vue: add new component

---
+++ src/views/layouts/SectionLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    defineProps<{
+        title: string
+    }>();
+</script>
+
+<template>
+    <section class="mb-16 xl:max-w-none">
+        <h2 class="scroll-mt-24 pb-3 border-b border-divider-light dark:border-divider-dark">{{ title }}</h2>
+        <slot />
+    </section>
+</template>
