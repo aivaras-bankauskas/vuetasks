936b03079da5b2974286ab8d0c130d9b873df33d
aivaras <aivarasbankauskas@gmail.com>
2023-12-18 12:38:58 +0200

    UserAvatar.vue: add ImageAvatar and AccountDropdown components

--- src/components/avatar/UserAvatar.vue
+++ src/components/avatar/UserAvatar.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
     import { computed } from 'vue';
-    import { useToggleStore } from '@/store/toggleStore';
-    import OnlineStatusBadge from '@/components/badges/OnlineStatusBadge.vue';
-    import UserPopupCard from '@/components/cards/UserPopupCard.vue';
+    import ImageAvatar from './ImageAvatar.vue';
+    import AccountDropdown from '../dropdowns/AccountDropdown.vue';
 
     const props = defineProps<{
         data: {
@@ -14,38 +13,30 @@
         }
     }>();
 
-    const toggleStore = useToggleStore();
     const onlineStatus = computed(() => props.data.onlineStatus);
 
     const fullName = computed(() => `${props.data.firstName} ${props.data.lastName}`);
-
-    const toggleUserPopup = (): void => {
-        toggleStore.toggleUserPopup();
-    };
 </script>
 
 <template>
     <div
-        :class="[
-            'h-8 w-8 hover:ring-4 hover:ring-black/5 dark:hover:ring-white/20 rounded-full cursor-pointer',
-            {
-                'hidden xxs:hidden sm:flex': toggleStore.isSearchShown,
-                'hidden xxs:flex lg:hidden': !toggleStore.isSearchShown
-            }
-        ]"
+        class="relative group hover:ring-4 hover:ring-black/5 dark:hover:ring-white/20 rounded-full cursor-pointer"
     >
         <RouterLink to="/account">
-            <div class="relative">
-                <img class="h-8 w-8 rounded-full" :src="props.data.avatar" :alt="fullName">
-                <OnlineStatusBadge :online-status="onlineStatus" position-class="w-2 h-2 absolute bottom-0 left-6" />
-            </div>
+            <ImageAvatar
+                class="h-8 w-8"
+                :img-source="props.data.avatar"
+                :alt-source="fullName"
+                :online-status="onlineStatus"
+                badge-position="w-2 h-2 absolute bottom-0 left-6"
+                online-badge
+            />
         </RouterLink>
+        <AccountDropdown
+            class="absolute top-6 right-0 hidden lg:group-hover:block lg:hover:block"
+            :full-name="fullName"
+            :email="props.data.email"
+            :avatar="props.data.avatar"
+        />
     </div>
-    <div class="h-8 w-8 hidden lg:flex hover:ring-4 hover:ring-black/5 dark:hover:ring-white/20 rounded-full cursor-pointer">
-        <button class="relative p-0 border-none background-transparent" @click="toggleUserPopup">
-            <img class="h-8 w-8 rounded-full" :src="props.data.avatar" :alt="fullName">
-            <OnlineStatusBadge :online-status="onlineStatus" position-class="w-2 h-2 absolute bottom-0 left-6" />
-        </button>
-    </div>
-    <UserPopupCard v-if="toggleStore.isUserPopupOpen" :full-name="fullName" :email="props.data.email" :avatar="props.data.avatar" />
 </template>
