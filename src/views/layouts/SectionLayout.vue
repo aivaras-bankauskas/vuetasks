f88000a13a34461955431a430fff7e4c419739d6
aivaras <aivarasbankauskas@gmail.com>
2023-12-19 19:51:09 +0200

    SectionLayout.vue: add actions and update pages

--- src/views/layouts/SectionLayout.vue
+++ src/views/layouts/SectionLayout.vue
@@ -1,12 +1,45 @@
 <script setup lang="ts">
-    defineProps<{
-        title: string
-    }>();
+    import { useToggleStore } from '@/store/toggleStore';
+    import SearchInput from '@/components/inputs/SearchInput.vue';
+    import SearchIcon from '@/components/icons/SearchIcon.vue';
+    import BaseButton from '@/components/buttons/BaseButton.vue';
+
+    defineProps({
+        title: {
+            type: String,
+            required: true
+        },
+        isActions: {
+            type: Boolean,
+            default: false
+        },
+        buttonText: {
+            type: String,
+            default: 'Create'
+        },
+        searchId: {
+            type: String,
+            default: ''
+        }
+    });
+
+    const toggleStore = useToggleStore();
 </script>
 
 <template>
-    <section class="mb-16 xl:max-w-none">
-        <h2 class="scroll-mt-24 pb-3 border-b border-divider-light dark:border-divider-dark">{{ title }}</h2>
+    <section class="mb-16">
+        <nav class="flex justify-between items-center scroll-mt-24 pb-3 border-b border-divider-light dark:border-divider-dark">
+            <h2 :class="{ 'hidden': toggleStore.isSearchShown[searchId] }">{{ title }}</h2>
+            <div v-if="isActions" :class="{ 'w-full': toggleStore.isSearchShown[searchId] }" class="flex justify-between items-center gap-4">
+                <div :class="{ 'hidden': !toggleStore.isSearchShown[searchId] }" class="sm:block max-w-full sm:max-w-xs flex-auto">
+                    <SearchInput :placeholder="`Search in ${title.toLowerCase()}...`" input-class="border border-divider-light dark:border-divider-dark" />
+                </div>
+                <SearchIcon :search-id="searchId" />
+                <RouterLink  to="/projects/create" :class="{ 'hidden': toggleStore.isSearchShown[searchId] }">
+                    <BaseButton type="button" :button-text="buttonText" class="btn-primary px-5 py-1 font-medium rounded-md text-sm text-white" />
+                </RouterLink>
+            </div>
+        </nav>
         <slot />
     </section>
 </template>
--- src/views/pages/team/TeamPage.vue
+++ src/views/pages/team/TeamPage.vue
@@ -6,7 +6,12 @@
 <template>
     <main class="flex-auto text-color-light dark:text-color-dark">
         <div class="flex h-full flex-col py-12">
-            <SectionLayout title="Team">
+            <SectionLayout
+                title="Team"
+                button-text="Add Member"
+                search-id="teamSearch"
+                is-actions
+            >
                 <div class="not-prose mt-8 rounded-2xl bg-light dark:bg-dark">
                     <ul role="list" class="divide-y divide-divider-light dark:divide-divider-dark px-4 py-2">
                         <li class="flex justify-between gap-x-6 py-5">
