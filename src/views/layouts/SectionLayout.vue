7dc8da46ada498c874ef0af4706db627c2528ee7
aivaras <aivarasbankauskas@gmail.com>
2023-12-20 09:11:03 +0200

    SectionLayout.vue: add  computed property

--- src/views/layouts/SectionLayout.vue
+++ src/views/layouts/SectionLayout.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+    import { computed } from 'vue';
     import { useToggleStore } from '@/store/toggleStore';
     import SearchInput from '@/components/inputs/SearchInput.vue';
     import SearchIcon from '@/components/icons/SearchIcon.vue';
     import BaseButton from '@/components/buttons/BaseButton.vue';
 
-    defineProps({
+    const props = defineProps({
         title: {
             type: String,
             required: true
@@ -24,6 +25,8 @@
     });
 
     const toggleStore = useToggleStore();
+
+    const titleToLowerCase = computed(() => props.title.toLowerCase());
 </script>
 
 <template>
@@ -32,10 +35,10 @@
             <h2 :class="{ 'hidden': toggleStore.isSearchShown[searchId] }">{{ title }}</h2>
             <div v-if="isActions" :class="{ 'w-full': toggleStore.isSearchShown[searchId] }" class="flex justify-between items-center gap-4">
                 <div :class="{ 'hidden': !toggleStore.isSearchShown[searchId] }" class="sm:block max-w-full sm:max-w-xs flex-auto">
-                    <SearchInput :placeholder="`Search in ${title.toLowerCase()}...`" input-class="border border-divider-light dark:border-divider-dark" />
+                    <SearchInput :placeholder="`Search in ${titleToLowerCase}...`" input-class="border border-divider-light dark:border-divider-dark" />
                 </div>
                 <SearchIcon :search-id="searchId" />
-                <RouterLink  to="/projects/create" :class="{ 'hidden': toggleStore.isSearchShown[searchId] }">
+                <RouterLink  :to="`/${titleToLowerCase}/create`" :class="{ 'hidden': toggleStore.isSearchShown[searchId] }">
                     <BaseButton type="button" :button-text="buttonText" class="btn-primary px-5 py-1 font-medium rounded-md text-sm text-white" />
                 </RouterLink>
             </div>
