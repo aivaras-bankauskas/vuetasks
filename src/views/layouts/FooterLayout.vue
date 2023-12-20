95b02fae988dee1d86f6e2c467401a449d6114aa
aivaras <aivarasbankauskas@gmail.com>
2023-12-20 10:24:23 +0200

    FooterLayout.vue: add BaseButton component

--- src/views/layouts/FooterLayout.vue
+++ src/views/layouts/FooterLayout.vue
@@ -1,27 +1,33 @@
+<script setup lang="ts">
+    import BaseButton from '@/components/buttons/BaseButton.vue';
+</script>
+
 <template>
     <footer class="mx-auto w-full space-y-10 pb-16">
         <div class="ml-auto flex justify-between gap-3">
             <RouterLink
-                class="footer-button link-hover"
                 aria-label="Previous: [Description of Previous Page]"
                 to="#"
             >
-                <svg viewBox="0 0 20 20" fill="none" aria-hidden="true" class="mt-0.5 h-5 w-5 -ml-1 rotate-180">
-                    <title>Previous</title>
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="m11.5 6.5 3 3.5m0 0-3 3.5m3-3.5h-9"></path>
-                </svg>
-                <span>Previous</span>
+                <BaseButton type="button" button-text="Previous" class="footer-button link-hover" title="Previous">
+                    <template #frontIcon>
+                        <svg viewBox="0 0 20 20" fill="none" aria-hidden="true" class="mt-0.5 h-5 w-5 -ml-1 rotate-180">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="m11.5 6.5 3 3.5m0 0-3 3.5m3-3.5h-9"></path>
+                        </svg>
+                    </template>
+                </BaseButton>
             </RouterLink>
             <RouterLink
-                class="footer-button link-hover"
                 aria-label="Next: [Description of Next Page]"
                 to="#"
             >
-                <span>Next</span>
-                <svg viewBox="0 0 20 20" fill="none" aria-hidden="true" class="mt-0.5 h-5 w-5 -mr-1">
-                    <title>Next</title>
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="m11.5 6.5 3 3.5m0 0-3 3.5m3-3.5h-9"></path>
-                </svg>
+                <BaseButton type="button" button-text="Next" class="footer-button link-hover" title="Next">
+                    <template #backIcon>
+                        <svg viewBox="0 0 20 20" fill="none" aria-hidden="true" class="mt-0.5 h-5 w-5 -mr-1">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="m11.5 6.5 3 3.5m0 0-3 3.5m3-3.5h-9"></path>
+                        </svg>
+                    </template>
+                </BaseButton>
             </RouterLink>
         </div>
         <div class="flex flex-col items-center justify-between gap-5 border-t border-zinc-900/5 pt-8 dark:border-gray-dark sm:flex-row">
@@ -32,6 +38,6 @@
 
 <style scoped>
     .footer-button {
-        @apply inline-flex gap-0.5 justify-center overflow-hidden text-sm font-medium transition rounded-full py-1 px-3 bg-light dark:bg-dark hover:bg-light/50 dark:hover:bg-dark/70 focus:ring-2 focus:ring-light focus:dark:ring-dark;
+        @apply flex gap-1 py-1 px-5 text-sm font-medium transition rounded-full bg-light dark:bg-dark hover:bg-light/50 dark:hover:bg-dark/70 focus:ring-2 focus:ring-light focus:dark:ring-dark;
     }
 </style>
