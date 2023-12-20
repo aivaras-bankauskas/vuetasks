28c27bc52cb9f3cbe96a886dd9fbc1d1cf5bef00
aivaras <aivarasbankauskas@gmail.com>
2023-12-20 17:45:31 +0200

    SidebarLayout.vue: optimize component for better readability

--- src/views/layouts/SidebarLayout.vue
+++ src/views/layouts/SidebarLayout.vue
@@ -13,42 +13,53 @@
                     <li v-for="(parent, parentIndex) in navigationRoutes" :key="parentIndex" class="relative">
                         <div class="navbar-text"><span class="truncate">{{ parent?.meta?.title }}</span></div>
                         <ul role="list">
-                            <NavigationLink
-                                v-for="(child, childIndex) in parent.children"
-                                :key="childIndex"
-                                :title="(child?.meta?.title as string)"
-                                :link-to="child?.path"
-                                show-border-if-active
-                            />
+                            <li v-for="(child, childIndex) in parent.children" :key="childIndex">
+                                <NavigationLink
+                                    class="navbar-upper-link hover:text-primary"
+                                    :title="(child?.meta?.title as string)"
+                                    :link-to="child?.path"
+                                    show-border-if-active
+                                />
+                            </li>
                         </ul>
                     </li>
                 </ul>
             </div>
         </li>
-        <NavigationLink title="Account" link-to="/account" list-class="xxs:hidden my-3" base-class="flex gap-x-3 justify-start items-center text-sm leading-5">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
-            </svg>
-        </NavigationLink>
-        <ThemeIcon class="md:hidden w-full flex gap-x-3 justify-start items-center text-sm my-3 hover:text-primary">
-            <span class="hidden dark:block text-sm">Dark Theme</span>
-            <span class="dark:hidden text-sm">Light Theme</span>
-        </ThemeIcon>
-        <NavigationLink title="API" link-to="/api" list-class="lg:hidden my-3" base-class="flex gap-x-3 justify-start items-center text-sm leading-5">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9.75v6.75m0 0l-3-3m3 3l3-3m-8.25 6a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
-            </svg>
-        </NavigationLink>
-        <NavigationLink title="Documentation" link-to="/documentation" list-class="lg:hidden my-3" base-class="flex gap-x-3 justify-start items-center text-sm leading-5">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
-            </svg>
-        </NavigationLink>
-        <NavigationLink title="Sign Out" link-to="/sign-out" list-class="lg:hidden mt-10" base-class="flex gap-x-3 justify-start items-center text-sm leading-5" text-color="text-danger hover:text-danger-light" active-text="text-danger">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
-            </svg>
-        </NavigationLink>
+        <li class="xxs:hidden my-3">
+            <NavigationLink title="Account" link-to="/account" class="navbar-lower-link hover:text-primary">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg>
+            </NavigationLink>
+        </li>
+        <li class="md:hidden my-3">
+            <ThemeIcon class="w-full flex gap-x-3 justify-start items-center text-sm hover:text-primary">
+                <span class="hidden dark:block text-sm">Dark Theme</span>
+                <span class="dark:hidden text-sm">Light Theme</span>
+            </ThemeIcon>
+        </li>
+        <li class="lg:hidden my-3">
+            <NavigationLink title="API" link-to="/api" class="navbar-lower-link hover:text-primary">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9.75v6.75m0 0l-3-3m3 3l3-3m-8.25 6a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
+                </svg>
+            </NavigationLink>
+        </li>
+        <li class="lg:hidden my-3">
+            <NavigationLink title="Documentation" link-to="/documentation" class="navbar-lower-link hover:text-primary">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+                </svg>
+            </NavigationLink>
+        </li>
+        <li class="lg:hidden mt-10">
+            <NavigationLink title="Sign Out" link-to="/sign-out" class="navbar-lower-link text-danger hover:text-danger-light" active-text="text-danger">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
+                </svg>
+            </NavigationLink>
+        </li>
     </ul>
 </template>
 
@@ -58,5 +69,8 @@
 }
 .navbar-upper-link {
     @apply flex justify-between gap-2 py-1 pr-3 text-sm transition pl-7;
+}
+.navbar-lower-link {
+    @apply flex gap-x-3 justify-start items-center text-sm leading-5;
 }
 </style>
