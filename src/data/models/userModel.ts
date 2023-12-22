d9db8017d2609a06540e74468f7c2694b2841256
aivaras <aivarasbankauskas@gmail.com>
2023-12-22 14:25:21 +0200

    userModel.ts: add User model

---
+++ src/data/models/userModel.ts
@@ -0,0 +1,14 @@
+type User = {
+    id: string;
+    avatar: string;
+    firstName: string;
+    lastName: string;
+    position: string;
+    onlineStatus: string;
+    email: string;
+    password?: string;
+    createdAt?: string;
+    updatedAt?: string;
+};
+
+export default User;
